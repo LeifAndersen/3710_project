424249ec95cd739308b362ab961320cfbbf7a6fe
Daniel Blakemore <DanBlakemore@gmail.com>
2011-09-13 10:36:04 -0600

    Git ignore and more comments.

--- ALU/ALU_tests.v
+++ ALU/ALU_tests.v
@@ -1,26 +1,26 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    17:07:52 09/11/2011 
-// Design Name: 
-// Module Name:    ALU_tests 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Company:
+// Engineer:
 //
-// Dependencies: 
+// Create Date:    17:07:52 09/11/2011
+// Design Name:
+// Module Name:    ALU_tests
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Revision: 
+// Dependencies:
+//
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ALU_tests(
     );
-	
+
 	// Inputs
 	reg [15:0] A;
 	reg [15:0] B;
@@ -37,10 +37,10 @@ module ALU_tests(
 	integer i;
 	// Instantiate the Unit Under Test (UUT)
 	ALU uut (
-		.A(A), 
-		.B(B), 
-		.C(C), 
-		.Opcode(Opcode), 
+		.A(A),
+		.B(B),
+		.C(C),
+		.Opcode(Opcode),
 		.Carry(Carry),
 		.Flag(Flag),
 		.Low(Low),
@@ -56,16 +56,24 @@ module ALU_tests(
 		Opcode = 2'b11;
 
 		//Random simulation
-		for( i = 0; i< 10; i = 1+ 1)
+        /*for( i = 0; i< 10; i = 1+ 1)
 		begin
 			#10
 			A = $random % 16;
 			B = $random %16;
-		end
+		end*/
+
+		// Edge Cases
+		// ADD
+
+
+		// ADDU
+
+
+		// SUB
+
+
 		$finish(2);
-		
-		// Edge cases
-		
 
 	end
 
