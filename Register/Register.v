126b38653db453802cedf1f1ad84391338ab9a32
Leif Andersen <leif@leifandersen.net>
2011-09-22 16:24:14 -0600

    Did register stuff.

--- Register/Register.v
+++ Register/Register.v
@@ -22,78 +22,106 @@
 
 module Register(
 		input clk,
-		input [3:0] A,
-		input [3:0] B,
-		input [3:0] C,
+		input [3:0] read_select_1,
+		input [3:0] read_select_2,
+		input [3:0] write_select,
 		input write,
 		input reset,
 		input [15:0] inputReg,
-		output reg [15:0] outputReg1,
-		output reg [15:0] outputReg2
+		output reg [15:0] output_reg_1,
+		output reg [15:0] output_reg_2
 	);
 
-	reg[15:0]R[15:0];
-
-always@(*) begin
-	case(A)
-	4'd0:outputReg1 = R[4'd0];
-	4'd1:outputReg1 = R[4'd1];
-	4'd2:outputReg1 = R[4'd2];
-	4'd3:outputReg1 = R[4'd3];
-	4'd4:outputReg1 = R[4'd4];
-	4'd5:outputReg1 = R[4'd5];
-	4'd6:outputReg1 = R[4'd6];
-	4'd7:outputReg1 = R[4'd7];
-	4'd8:outputReg1 = R[4'd8];
-	4'd9:outputReg1 = R[4'd9];
-	4'd10:outputReg1 = R[4'd10];
-	4'd11:outputReg1 = R[4'd11];
-	4'd12:outputReg1 = R[4'd12];
-	4'd13:outputReg1 = R[4'd13];
-	4'd14:outputReg1 = R[4'd14];
-	default:outputReg1 = R[4'd15];
-	endcase
-
-	case(B)
-	4'd0:outputReg2 = R[4'd0];
-	4'd1:outputReg2 = R[4'd1];
-	4'd2:outputReg2 = R[4'd2];
-	4'd3:outputReg2 = R[4'd3];
-	4'd4:outputReg2 = R[4'd4];
-	4'd5:outputReg2 = R[4'd5];
-	4'd6:outputReg2 = R[4'd6];
-	4'd7:outputReg2 = R[4'd7];
-	4'd8:outputReg2 = R[4'd8];
-	4'd9:outputReg2 = R[4'd9];
-	4'd10:outputReg2 = R[4'd10];
-	4'd11:outputReg2 = R[4'd11];
-	4'd12:outputReg2 = R[4'd12];
-	4'd13:outputReg2 = R[4'd13];
-	4'd14:outputReg2 = R[4'd14];
-	default:outputReg2 = R[4'd15];
-	endcase
-end
+	reg[15:0] reg0;
+	reg[15:0] reg1;
+	reg[15:0] reg2;
+	reg[15:0] reg3;
+	reg[15:0] reg4;
+	reg[15:0] reg5;
+	reg[15:0] reg6;
+	reg[15:0] reg7;
+	reg[15:0] reg8;
+	reg[15:0] reg9;
+	reg[15:0] reg10;
+	reg[15:0] reg11;
+	reg[15:0] reg12;
+	reg[15:0] reg13;
+	reg[15:0] reg14;
+	reg[15:0] reg15;
 
 always@(posedge clk) begin
 	if(reset == 1'b1) begin
-		R[0] = 0;
-		R[1] = 0;
-		R[2] = 0;
-		R[3] = 0;
-		R[4] = 0;
-		R[5] = 0;
-		R[6] = 0;
-		R[7] = 0;
-		R[8] = 0;
-		R[9] = 0;
-		R[10] = 0;
-		R[14] = 0;
-		R[15] = 0;
+		reg0 = 0;
+		reg1 = 0;
+		reg2 = 0;
+		reg3 = 0;
+		reg4 = 0;
+		reg5 = 0;
+		reg6 = 0;
+		reg7 = 0;
+		reg8 = 0;
+		reg9 = 0;
+		reg10 = 0;
+		reg11 = 0;
+		reg12 = 0;
+		reg13 = 0;
+		reg14 = 0;
+		reg15 = 0;
+	end else if(write == 1'b1) begin
+		case(write_select)
+			4'd0:reg0 = inputReg;
+			4'd1:reg1 = inputReg;
+			4'd2:reg2 = inputReg;
+			4'd3:reg3 = inputReg;
+			4'd4:reg4 = inputReg;
+			4'd5:reg5 = inputReg;
+			4'd6:reg6 = inputReg;
+			4'd7:reg7 = inputReg;
+			4'd8:reg8 = inputReg;
+			4'd9:reg9 = inputReg;
+			4'd10:reg10 = inputReg;
+			4'd11:reg11 = inputReg;
+			4'd12:reg12 = inputReg;
+			4'd13:reg13 = inputReg;
+			4'd14:reg14 = inputReg;
+			4'd15:reg15 = inputReg;
+		endcase
 	end
-	else if(write == 1'b1)
-		begin
-			R[C] = inputReg;
-		end
-	end
-
+	case(read_select_1)
+		4'd0:output_reg_1 = reg0;
+		4'd1:output_reg_1 = reg1;
+		4'd2:output_reg_1 = reg2;
+		4'd3:output_reg_1 = reg3;
+		4'd4:output_reg_1 = reg4;
+		4'd5:output_reg_1 = reg5;
+		4'd6:output_reg_1 = reg6;
+		4'd7:output_reg_1 = reg7;
+		4'd8:output_reg_1 = reg8;
+		4'd9:output_reg_1 = reg9;
+		4'd10:output_reg_1 = reg10;
+		4'd11:output_reg_1 = reg11;
+		4'd12:output_reg_1 = reg12;
+		4'd13:output_reg_1 = reg13;
+		4'd14:output_reg_1 = reg14;
+		4'd15:output_reg_1 = reg15;
+	endcase
+	case(read_select_2)
+		4'd0:output_reg_2 = reg0;
+		4'd1:output_reg_2 = reg1;
+		4'd2:output_reg_2 = reg2;
+		4'd3:output_reg_2 = reg3;
+		4'd4:output_reg_2 = reg4;
+		4'd5:output_reg_2 = reg5;
+		4'd6:output_reg_2 = reg6;
+		4'd7:output_reg_2 = reg7;
+		4'd8:output_reg_2 = reg8;
+		4'd9:output_reg_2 = reg9;
+		4'd10:output_reg_2 = reg10;
+		4'd11:output_reg_2 = reg11;
+		4'd12:output_reg_2 = reg12;
+		4'd13:output_reg_2 = reg13;
+		4'd14:output_reg_2 = reg14;
+		4'd15:output_reg_2 = reg15;
+	endcase
+end
 endmodule
