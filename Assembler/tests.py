4ce439a25f5ad14c568128e9a81d86df80dcbf93
Leif Andersen <leif@leifandersen.net>
2011-10-22 12:11:05 -0600

    Updated tests.py

--- Assembler/tests.py
+++ Assembler/tests.py
@@ -23,36 +23,62 @@ class AssemblerTestCase(unittest.TestCase):
 		self.infile = open(self.infileName, 'w')
 		self.infile.write('''
 # simple loop test with no stack usage.  Starts heap from bottom
-	MOV 7, $0
-	MOV 13, $1
-	MOV 14, $4
-	MOV 42, $2
+	MOV $0, 7
+	MOV $1, 13
+	MOV $4, 14
+	MOV $2, 42
 
 TESTPARITY:
-	MOV $4, $3
-	AND 1, $3
-	CMP 0, $3
+	MOV $3, $4
+	AND $3, 1
+	CMP $3, 0
 	JE EVEN 		# Jump if $4 is even
-	ADD $1, $0
+	ADD $0, $1
 	DECR $4
-	CMP 0, $4
+	CMP $4, 0
 	JNE TESTPARITY
 	JE WASTELOOP
 
 EVEN:
-	ADD $0, $1
+	ADD $1, $0
 	DECR $4
-	CMP $4, $0		# other kind of compare
+	CMP 0, $4		# other kind of compare
 	JNE TESTPARITY
 
 WASTELOOP:
-	SUB 0, $1	# 1984 is now the current fib value in hex.  Hold it on the write line
-	OR 0, $1
-	NOT $5
+	SUB $1, 0	# 1984 is now the current fib value in hex.  Hold it on the write line
+	OR $1, 0
+	NOT $5, 0
 	JNE WASTELOOP
 ''')
 		self.infile.close()
 		assembler.parse(self.infileName, self.outfileName)
+		self.outfile = open(self.outfileName, 'r')
+		out_str = self.outfile.read()
+		self.outfile.close()
+		test_str = '''f007
+f10d
+f40e
+f22a
+3f4
+6301
+4300
+2c00d
+21
+3d400
+4400
+28004
+2c011
+120
+3d400
+5400
+28004
+3100
+7100
+9500
+28011
+'''
+		assert out_str == test_str
 
 def main():
 	unittest.main()
