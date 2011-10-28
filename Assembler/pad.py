eee2206fba61ade97891081a6453dea15b72fb04
Daniel Blakemore <DanBlakemore@gmail.com>
2011-10-27 18:46:16 -0600

    use pad.py with the .o file from the assembler and the size of memory (number of addresses) as args

---
+++ Assembler/pad.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+from os.path import splitext
+
+def pad(infile_str, outfile_str, size):
+	infile = open(infile_str, 'r')
+	outfile = open(outfile_str, 'w')
+	for line in infile:
+		if len(line) > 0:
+			outfile.write(line)
+			size -= 1
+
+	for i in range(size):
+		outfile.write("0\n")
+
+	infile.close()
+	outfile.close()
+
+def main():
+	filestring = sys.argv[1]
+	filenameparts = splitext(filestring)
+	outfile = filenameparts[0] + ".mem"
+	size = int(sys.argv[2])
+	pad(filestring, outfile, size)
+
+if __name__ == '__main__':
+	main()
