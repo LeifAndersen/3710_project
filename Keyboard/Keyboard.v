60c0a83a006d85473a36371a356f01c79b7e4ed9
Leif Andersen <leif@leifandersen.net>
2011-11-02 22:01:23 -0600

    Modified the keyboard.

--- Keyboard/Keyboard.v
+++ Keyboard/Keyboard.v
@@ -22,12 +22,18 @@ module Keyboard(
     input clk,
     input data,
 	 input reset,
-    output reg[16:0] buttons
+    output reg[15:0] up,
+	 output reg[15:0] down,
+	 output reg[15:0] left,
+	 output reg[15:0] right,
+	 output reg[15:0] a,
+	 output reg[15:0] b
 	 );
 	 
 `include "KeyboardCodes.v"
 
 reg[10:0] shiftReg;
+reg[5:0] buttons;
 
 reg rel;
 
@@ -65,6 +71,25 @@ begin
 			rel = 1'b0;
 		end
 		shiftReg = 11'b11111111111;
+	end
+	
+	if(buttons[0] == 1'b1) begin
+		up = up + 1;
+	end
+	if(buttons[1] == 1'b1) begin
+		down = down + 1;
+	end
+	if(buttons[2] == 1'b1) begin
+		left = left + 1;
+	end
+	if(buttons[3] == 1'b1) begin
+		right = right + 1;
+	end
+	if(buttons[4] == 1'b1) begin
+		a = a + 1;
+	end
+	if(buttons[5] == 1'b1) begin
+		b = b + 1;
 	end
 end
 
--- Keyboard/KeyboardCodes.v
+++ Keyboard/KeyboardCodes.v
@@ -18,10 +18,10 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-parameter UP = 11'b01111111111;
-parameter DOWN = 2;
-parameter LEFT = 4;
-parameter RIGHT = 6;
-parameter A = 8;
-parameter B = 10;
-parameter REL = 12;
+parameter UP = 11'b01011100011;
+parameter DOWN = 11'b01101100011;
+parameter LEFT = 11'b00011100001;
+parameter RIGHT = 11'b01100010001;
+parameter A = 11'b01001010001;
+parameter B = 11'b00100100011;
+parameter REL = 11'b00000111111;
