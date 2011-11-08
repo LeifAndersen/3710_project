eaef5dea263948c4b9cc491fc15f2b5cc0023ae9
Leif Andersen <leif@leifandersen.net>
2011-11-08 13:09:44 -0700

    Very simple assembly code, it doesn't work...

---
+++ Program/code.asm
@@ -0,0 +1,12 @@
+# Bootup and initialization Code
+init:
+
+# Main Loop
+main:
+	call main
+
+# Take a number in the $1 reg, return the sin of that number into the $1 reg
+sin:
+
+# Take a number in the $1 reg, return the cos of that number into the $1 reg
+cos:
