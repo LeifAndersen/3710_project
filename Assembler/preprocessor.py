37dd784a88498ebba29bfe038d1951affb8bc23b
Leif Andersen <leif@leifandersen.net>
2011-11-08 17:19:05 -0700

    Debugged preprocessor

--- Assembler/preprocessor.py
+++ Assembler/preprocessor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 from os.path import splitext
 from os.path import dirname
@@ -14,23 +16,25 @@ def parse(infile_name, outfile_name):
 		line_tokens = line.split()
 		if len(line_tokens) == 0:
 			continue
-		if line_tokens[0] = '`':
+		if line_tokens[0][0] == '`':
 			if len(line_tokens) != 3:
 				print "Incoredt preprocessor command on line " + str(i) + "."
-			tokens[tokens[1]] = tokens[2]
+			tokens[line_tokens[1]] = line_tokens[2]
 		i+=1
 
 	# Kill the pre-processor commands
 	infile.seek(0)	
 	for line in infile:
 		line_tokens = line.split()
-		if len(line_tokens) == 0 or line_tokens[0] == '`':
+		if len(line_tokens) == 0 or line_tokens[0][0] == '`':
 			continue
 
 		line_out = ""
 		for token in line_tokens:
 			if token in tokens:
 				line_out += tokens[token]
+			elif token[0] == '[' and token[-1] == ']' and token[1:-1] in tokens:
+				line_out += '[' + tokens[token[1:-1]] + ']'
 			else:
 				line_out += token
 			line_out += " "
