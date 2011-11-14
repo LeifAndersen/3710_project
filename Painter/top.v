5520fb4e4b161ddada5b70f370474d8b7d81754b
unknown <jhparker@.(none)>
2011-11-13 23:41:17 -0700

    Changed DrawUnit's top module to use a DCM, works just fine first try! -JP

--- Painter/top.v
+++ Painter/top.v
@@ -26,8 +26,8 @@ module top(
 	output vsync
     );
 
-reg vgaClk;
-//wire clkfb;
+wire vgaClk;
+wire clkfb;
 reg we;
 reg[15:0] data;
 reg[9:0] i;
@@ -44,10 +44,10 @@ begin
 		we <= 0;
 		state <= 0;
 		cycles <= 0;
-		vgaClk <= 0;
+		//vgaClk <= 0;
 	end
 	else begin
-		vgaClk <= ~vgaClk;
+		//vgaClk <= ~vgaClk;
 		if (i < 255)
 		begin
 			case(state)
@@ -99,29 +99,29 @@ begin
 		end
 	end
 end
-//
-//DCM_SP #(
-//      .CLKDV_DIVIDE(2.0), // Divide by: 1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5
-//                          //   7.0,7.5,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0 or 16.0
-//      //.CLKFX_DIVIDE(1),   // Can be any integer from 1 to 32
-//      //.CLKFX_MULTIPLY(4), // Can be any integer from 2 to 32
-//      .CLKIN_DIVIDE_BY_2("FALSE"), // TRUE/FALSE to enable CLKIN divide by two feature
-//      //.CLKIN_PERIOD(0.0),  // Specify period of input clock
-//      .CLKOUT_PHASE_SHIFT("NONE"), // Specify phase shift of NONE, FIXED or VARIABLE
-//      .CLK_FEEDBACK("1X"),  // Specify clock feedback of NONE, 1X or 2X
-//      .DESKEW_ADJUST("SYSTEM_SYNCHRONOUS"), // SOURCE_SYNCHRONOUS, SYSTEM_SYNCHRONOUS or
-//                                            //   an integer from 0 to 15
-//      .DLL_FREQUENCY_MODE("LOW"),  // HIGH or LOW frequency mode for DLL
-//      .DUTY_CYCLE_CORRECTION("TRUE"), // Duty cycle correction, TRUE or FALSE
-//      .PHASE_SHIFT(0),     // Amount of fixed phase shift from -255 to 255
-//      .STARTUP_WAIT("TRUE")   // Delay configuration DONE until DCM LOCK, TRUE/FALSE
-//   ) DCM_SP_inst (
-//		.CLK0(clkfb),
-//      .CLKDV(vgaClk),   // Divided DCM CLK out (CLKDV_DIVIDE)
-//		.CLKFB(clkfb),
-//      .CLKIN(clk),   // Clock input (from IBUFG, BUFG or DCM)
-//      .RST(reset)        // DCM asynchronous reset input
-//   );
+
+DCM_SP #(
+      .CLKDV_DIVIDE(2.0), // Divide by: 1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5
+                          //   7.0,7.5,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0 or 16.0
+      //.CLKFX_DIVIDE(1),   // Can be any integer from 1 to 32
+      //.CLKFX_MULTIPLY(4), // Can be any integer from 2 to 32
+      .CLKIN_DIVIDE_BY_2("FALSE"), // TRUE/FALSE to enable CLKIN divide by two feature
+      //.CLKIN_PERIOD(0.0),  // Specify period of input clock
+      .CLKOUT_PHASE_SHIFT("NONE"), // Specify phase shift of NONE, FIXED or VARIABLE
+      .CLK_FEEDBACK("1X"),  // Specify clock feedback of NONE, 1X or 2X
+      .DESKEW_ADJUST("SYSTEM_SYNCHRONOUS"), // SOURCE_SYNCHRONOUS, SYSTEM_SYNCHRONOUS or
+                                            //   an integer from 0 to 15
+      .DLL_FREQUENCY_MODE("LOW"),  // HIGH or LOW frequency mode for DLL
+      .DUTY_CYCLE_CORRECTION("TRUE"), // Duty cycle correction, TRUE or FALSE
+      .PHASE_SHIFT(0),     // Amount of fixed phase shift from -255 to 255
+      .STARTUP_WAIT("TRUE")   // Delay configuration DONE until DCM LOCK, TRUE/FALSE
+   ) DCM_SP_inst (
+		.CLK0(clkfb),
+      .CLKDV(vgaClk),   // Divided DCM CLK out (CLKDV_DIVIDE)
+		.CLKFB(clkfb),
+      .CLKIN(clk),   // Clock input (from IBUFG, BUFG or DCM)
+      .RST(reset)        // DCM asynchronous reset input
+   );
 
  DrawUnit drawunit(
 	.clk(clk),
