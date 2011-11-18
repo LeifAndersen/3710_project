d6df29fa362bcb843c91834a72adca75830883c8
Daniel Blakemore <DanBlakemore@gmail.com>
2011-11-17 17:49:14 -0700

    11 Block rams, new padformemory, and new init files

--- Assembler/padformemory.py
+++ Assembler/padformemory.py
@@ -13,48 +13,58 @@ def print_percentage(percentage):
 		sys.stdout.write("\b\b\b\b\b")
 	sys.stdout.write("%d%%]" % (percentage) )
 
-def pad(infile_str, outfile_str, size):
+def pad(infile_str, size):
 	infile = open(infile_str, 'r')
-	outfile = open(outfile_str, 'w')
 
-	sys.stdout.write("\n\t(1/2)    Copying: [  0%]")
-	sys.stdout.flush()
+	outfiles = []
+	for i in range(0, int(size) / 1024):
+		outfiles.append(open("init" + str(i) + ".txt", 'w'))
 
-	line_count = len(infile.readlines())
-	line_num = 0
-
-	infile.seek(0)
-	for line in infile:
-		if len(line) > 0:
-			outfile.write(line)
-			size -= 1
-		line_num += 1
-		print_percentage(100 * line_num/float(line_count))
-		sys.stdout.flush()
-
-	sys.stdout.write("\n\t(2/2)    Padding: [  0%]")
+	sys.stdout.write("\n\t(1/1)    Padding: [  0%]")
 	sys.stdout.flush()
 
 	line_count = size
 	line_num = 0
 
+	outfile = outfiles[0]
+	filepos = 0
+	n = 0
+
+	for line in infile:
+		if filepos > 1023:
+			filepos = 0
+			n += 1
+			outfile = outfiles[n]
+		if len(line) > 0:
+			outfile.write(line)
+			size -= 1
+		line_num += 1
+		filepos += 1
+		print_percentage(100 * line_num/float(line_count))
+		sys.stdout.flush()
+
 	for i in range(size):
+		if filepos > 1023:
+			filepos = 0
+			n += 1
+			outfile = outfiles[n]
 		outfile.write("0\n")
 		line_num += 1
+		filepos += 1
 		print_percentage(100 * line_num/float(line_count))
 		sys.stdout.flush()
 
 	sys.stdout.write("\n")
 
 	infile.close()
-	outfile.close()
+	for i in range(0, int(size) / 1024):
+		outfiles[i].close()
 
 def main():
 	filestring = sys.argv[1]
 	filenameparts = splitext(filestring)
-	outfile = filenameparts[0] + ".mem"
 	size = int(sys.argv[2])
-	pad(filestring, outfile, size)
+	pad(filestring, size)
 
 if __name__ == '__main__':
 	main()
