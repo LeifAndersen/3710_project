65b9d253a2e656a4f4d05e96bd91d4d20a3cbf1e
unknown <jhparker@.(none)>
2011-11-22 23:10:07 -0700

    Fixed one of the bugs in drawunit, test program makes green screen with red corner points to test boundaries.  -JP

--- Program/JonScreenFill.asm
+++ Program/JonScreenFill.asm
@@ -17,7 +17,7 @@ and %5, %5
 
 mov %1, 2
 mov %2, 120
-lsh %2, 2
+lsh %2, 3
 add %2, 2
 mov %3, 0x9F
 mov %4, 0xFF
@@ -30,6 +30,34 @@ mov [VGA], %3
 add %1, 8
 cmp %1, %2
 jne loop
+
+mov %1, 0x7
+mov [VGA], %1
+mov %1, 0x0
+mov [VGA], %1
+
+mov %1, 0x7
+mov [VGA], %1
+mov %1, 159
+lsh %1, 8
+or %1, 159
+mov [VGA], %1
+
+mov %1, 119
+lsh %1, 3
+or %1, 7
+mov [VGA], %1
+mov %1, 0x0
+mov [VGA], %1
+
+mov %1, 119
+lsh %1, 3
+or %1, 7
+mov [VGA], %1
+mov %1, 159
+lsh %1, 8
+or %1, 159
+mov [VGA], %1
 
 mov [VGA], %4
 mov [VGA], %4
