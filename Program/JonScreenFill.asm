2f01e6d4854d142d7b412365936f126f4cf04077
unknown <u0595680@.dsl.cs.utah.edu>
2011-11-22 17:37:50 -0700

    Changed jonscreenfill -JP

--- Program/JonScreenFill.asm
+++ Program/JonScreenFill.asm
@@ -1,15 +1,15 @@
 `define VGA 16383
 
-mov %1 2
-mov %2 120
-lsh %2 3
-mov %3 0xC0
+mov %1, 2
+mov %2, 120
+lsh %2, 3
+mov %3, 0xA0
 
 loop:
-add %1 8
-mov [VGA] %1
-mov [VGA] %3
-jne %1 %2 loop
+add %1, 8
+mov [VGA], %1
+mov [VGA], %3
+jne %1, %2, loop
 
 infinite:
 j infinite
