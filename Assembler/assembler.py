c63a26e62960402018fe9be66876ab227f87c9b4
Daniel Blakemore <DanBlakemore@gmail.com>
2011-11-30 00:40:03 -0700

    Was not reversing jump logic correctly

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -245,25 +245,25 @@ def parse(infile_str, outfile_str):
 						# jmp
 						# NOP
 						first_pass_queue.append(encode_cmps(["CMPR", tokens[1], tokens[2]], line_num, line))
-						first_pass_queue.append("JL " + tokens[3])
+						first_pass_queue.append("JLE " + tokens[3])
 					elif tokens[0] == "JGE":
 						# CMPR
 						# jmp
 						# NOP
 						first_pass_queue.append(encode_cmps(["CMPR", tokens[1], tokens[2]], line_num, line))
-						first_pass_queue.append("JLE " + tokens[3])
+						first_pass_queue.append("JL " + tokens[3])
 					elif tokens[0] == "JA":
 						# CMPR
 						# jmp
 						# NOP
 						first_pass_queue.append(encode_cmps(["CMPR", tokens[1], tokens[2]], line_num, line))
-						first_pass_queue.append("JB " + tokens[3])
+						first_pass_queue.append("JBE " + tokens[3])
 					elif tokens[0] == "JAE":
 						# CMPR
 						# jmp
 						# NOP
 						first_pass_queue.append(encode_cmps(["CMPR", tokens[1], tokens[2]], line_num, line))
-						first_pass_queue.append("JBE " + tokens[3])
+						first_pass_queue.append("JB " + tokens[3])
 					elif tokens[0] == "JB":
 						# CMPR
 						# jmp
@@ -293,13 +293,13 @@ def parse(infile_str, outfile_str):
 						# jmp
 						# NOP
 						first_pass_queue.append(encode_cmps(["CMP", tokens[1], tokens[2]], line_num, line))
-						first_pass_queue.append("JLE " + tokens[3])
+						first_pass_queue.append("JE " + tokens[3])
 					elif tokens[0] == "JNE":
 						# CMPR
 						# jmp
 						# NOP
 						first_pass_queue.append(encode_cmps(["CMP", tokens[1], tokens[2]], line_num, line))
-						first_pass_queue.append("JLE " + tokens[3])
+						first_pass_queue.append("JNE " + tokens[3])
 					else:
 						explode_bomb(line_num, line)
 					first_pass_queue.append(str(hex(0)))
