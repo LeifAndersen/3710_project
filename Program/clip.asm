8081872eaaa7efa6b15b390a6afb28c16aff783f
unknown <jhparker@.(none)>
2011-12-03 19:41:58 -0700

    Started on z-clipping.  About half-way done. -JP

--- Program/clip.asm
+++ Program/clip.asm
@@ -396,9 +396,194 @@ clip:
 
 ### Z CLIPPING
 ###
-### Expects a 3-d triangle to be in the triangle array.
+### Expects a 3-d triangle to be in the triangle array. Expects a pointer to a triangle in eax.  Triangle is expected to be [eax] = color, [eax+1] = x1, [eax+2] = y1, [eax+3] = z1, [eax+4] = x2, [eax+5] = y2, [eax+66] = z2, [eax+7] = x3, [eax+8] = y3, [eax+9] = z3
 ###
 ###
+zclip:
+		push eax
+	
+		mov zone1, 0
+		mov zone2, 0
+		mov zone3, 0
+		
+		add eax, 3 #point eax at z1
+		mov ebx, [eax]
+		jge ebx, 0, point1zpositive
+			mov zone1, 1
+		point1zpositive:
+		
+		add eax, 3 #point eax at z2
+		mov ebx, [eax]
+		jge ebx, 0, point2zpositive
+			mov zone2, 1
+		point2zpositive:
+		
+		add eax, 3 #point eax at z3
+		mov ebx, [eax]
+		jge ebx, 0, point3zpositive
+			mov zone3, 1
+		point3zpositive:
+		
+		
+		###
+		### Z BASE CASE
+		###
+		mov eax, zone1
+		and eax, zone2
+		and eax, zone3
+		
+		je eax, 0, zbasecase1fail
+			ret
+		zbasecase1fail:
+		
+		mov eax, zone1
+		or eax, zone2
+		or eax, zone3
+		
+		jne eax, 0, zbasecase2fail
+			call rasterize
+			ret
+		zbasecase2fail:
+		
+		###
+		### Z REDUCTION STEP
+		###
+		
+		### At least one point is in znegative. Could be two points.
+		### No longer need to save this zone information, so we can modify zone1, zone2, zone3
+		
+		mov eex, 0 #Prepare eex for split, eex is a param for it.
+		
+		#Six possibilities on which points are in z negative.
+		jne zone1, 1, point1notin0
+			#point 1 is in 0
+			jne zone2, 1, point1in0point2out
+				# points 1 and 2 are in 0, point 3 is the pivot point.  Already presorted.
+				j split
+			point1in0point2out:
+			jne zone3, 1, point1in0point3out
+				# points 1 and 3 are in 0, point 2 is the pivot point.
+				# Must sort so point 3 is the pivot. Swap points 2 and 3
+				mov eax, [triangle+3]
+				mov ebx, [triangle+5]
+				mov [triangle+3], ebx
+				mov [triangle+5], eax
+				mov eax, [triangle+4]
+				mov ebx, [triangle+6]
+				mov [triangle+4], ebx
+				mov [triangle+6], eax
+				
+				j split				
+			point1in0point3out:
+			#Only point 1 is in 0, point 1 is the pivot point. Must swap point 1 and 3.
+				mov eax, [triangle+1]
+				mov ebx, [triangle+5]
+				mov [triangle+1], ebx
+				mov [triangle+5], eax
+				mov eax, [triangle+2]
+				mov ebx, [triangle+6]
+				mov [triangle+2], ebx
+				mov [triangle+6], eax
+				
+				j split			
+			point1notin0:
+			jne zone2, 1, point2notin0
+				#point 2 is in 0
+				jne zone3, 1, point2in0point3out
+					#points 2 and 3 are in 0. 1 is pivot.  Swap 1 and 3.
+					mov eax, [triangle+1]
+					mov ebx, [triangle+5]
+					mov [triangle+1], ebx
+					mov [triangle+5], eax
+					mov eax, [triangle+2]
+					mov ebx, [triangle+6]
+					mov [triangle+2], ebx
+					mov [triangle+6], eax
+					
+					j split
+			point2in0point3out:
+				#point 2 is the pivot.  Swap 2 and 3.
+				mov eax, [triangle+3]
+				mov ebx, [triangle+5]
+				mov [triangle+3], ebx
+				mov [triangle+5], eax
+				mov eax, [triangle+4]
+				mov ebx, [triangle+6]
+				mov [triangle+4], ebx
+				mov [triangle+6], eax
+				
+				j split
+			point2notin0:
+				#point 3 is the pivot. Already sorted.
+					j split
+					
+					###
+	### SPLIT
+	###
+	zsplit:
+		#points 1 and 2 are in 0, point 3 is the pivot point.
+				mov eax, [triangle+5]
+				mov ebx, [triangle+6]
+				mov ecx, [triangle+1]
+				mov edx, [triangle+2]
+				mov eex, 0
+				call binarySubdivide
+				mov zone1, eax # Zone1 contains the x-value of p1-p3 edge on y=0.
+				
+				mov eax, [triangle+5]
+				mov ebx, [triangle+6]
+				mov ecx, [triangle+3]
+				mov edx, [triangle+4]
+				mov eex, 0
+				call binarySubdivide #eax contains the x-value of p2-p3 edge on y=0.
+				
+				#Now form the three new triangles and push them onto the stack, recursive call to clip.
+				mov ebx, [triangle+2]
+				push ebx
+				mov ebx, [triangle+1]
+				push ebx
+				push 0
+				push zone1
+				push 0
+				push eax
+				
+				mov ebx, [triangle+2]
+				push ebx
+				mov ebx, [triangle+1]
+				push ebx
+				mov ebx, [triangle+4]
+				push ebx
+				mov ebx, [triangle+3]
+				push ebx
+				push 0
+				push eax
+				
+				mov ebx, [triangle+6]
+				push ebx
+				mov ebx, [triangle+5]
+				push ebx
+				push 0
+				push zone1
+				push 0
+				push eax
+				
+				mov eax, SP
+				decr eax
+				call clip
+				
+				sub SP, 6
+				mov eax, SP
+				decr eax
+				call clip
+				
+				sub SP, 6
+				mov eax, SP
+				decr eax
+				call clip
+				
+				sub SP, 6
+				
+				ret			
 
 ###
 ###
