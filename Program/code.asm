6f1939d19da3585ee4e9c02c13869c74bcf3eea0
Leif Andersen <leif@leifandersen.net>
2011-12-04 22:10:09 -0700

    Foos

--- Program/code.asm
+++ Program/code.asm
@@ -94,12 +94,12 @@ mainLoop:
 	mov %6, [DOWN_KEY]
 	sub %5, %6             # Up-Down now in %5
 	mov %0, %4             #
-	call sin               # %1 has sin(theta)
+	call cos               # %1 has sin(theta)
 	fmul %0, %5            #
 	add %2, %0             # Player X now updated by the move amount
 	                       #
 	mov %0, %4             #
-	call cos               # %1 has cos(theta)
+	call sin               # %1 has cos(theta)
 	fmul %0, %5            # %LOW/HIGH has (UP-DOWN)*cos(theta)
 	add %4, %0
 	mov [PLAYER_THETA], %4 # Save the theta
