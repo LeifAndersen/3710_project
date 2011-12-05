73566b0b9122fe3ea8c5e40bde15ae64aa6f38c5
Daniel Blakemore <DanBlakemore@gmail.com>
2011-12-04 21:10:17 -0700

    Now shows negative and doesn't explode on sign bit.

--- Assembler/fixedpointconverter.py
+++ Assembler/fixedpointconverter.py
@@ -23,31 +23,41 @@ def main():
 	if is_binary(sys.argv[1]):
 		pw = 0
 		y = 0
-		numstr = (bin(int(sys.argv[1], 2))[2:]).zfill(16)
+		numstr = (bin(int(sys.argv[1], 2))[2:]).zfill(15)
 		for char in numstr:
 			if char == '1':
 				y += (1 / float(two_to_the(pw)))
 			else:
 				pass
 			pw += 1
+
+		if int(sys.argv[1], 2) > 2**15:
+			y *= -1
 		print y
 
 	elif is_hex(sys.argv[1]):
 		pw = 0
 		y = 0
-		numstr = (bin(int(sys.argv[1], 16))[2:]).zfill(16)
+		numstr = (bin(int(sys.argv[1], 16))[2:]).zfill(15)
 		for char in numstr:
 			if char == '1':
 				y += (1 / float(two_to_the(pw)))
 			else:
 				pass
 			pw += 1
+
+		if int(sys.argv[1], 16) > 2**15:
+			y *= -1
 		print y
 
 	# if decimal or hex convert to binary
 	else:
 		y = float(sys.argv[1])
-		for j in map(two_to_the, range(0, 15)):
+		if y < 0:
+			sys.stdout.write("1")
+		else:
+			sys.stdout.write("0")
+		for j in map(two_to_the, range(0, 14)):
 			if (y - (1/float(j))) >= 0:
 				y -= 1 / float(j)
 				sys.stdout.write("1")
